c5d2d6c5b90e20b2d741428af8046df1e936ee19
AxVol <ax_vol@mail.ru>
2022-12-30 19:16:53 +0300

    Hide bot token, check and create database

---
+++ DiscordBot/DiscordBot/DateBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace DiscordBot
+{
+    class DateBase
+    {
+        private static readonly string dirName = "Documents";
+
+        public static string GetToken()
+        {
+            try
+            {
+                if (!Directory.Exists(dirName))
+                {
+                    Directory.CreateDirectory(dirName);
+                }
+
+                using (FileStream file = new FileStream("Documents/token.txt", FileMode.OpenOrCreate))
+                using (StreamReader stream = new StreamReader(file))
+                {
+                    string token = stream.ReadToEnd();
+                    return token;
+                }
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        public static void CreateDateBase(string serverName)
+        {
+            string path = $"{dirName}/{serverName}.db";
+
+            if (File.Exists(path))
+                return;
+                
+            var f = File.Create(path);
+            f.Close();
+        }
+    }
+}
--- DiscordBot/DiscordBot/Program.cs
+++ DiscordBot/DiscordBot/Program.cs
@@ -1,13 +1,17 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using Discord.Commands;
 using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DiscordBot
 {
     class Program
     {
         private DiscordSocketClient client;
+        private CommandService commands;
+        private IServiceProvider services;
 
         static Task Main() => new Program().MainAsync();
 
@@ -22,22 +26,27 @@ namespace DiscordBot
             client.MessageReceived += CommandsHandler;
             client.Log += Log;
 
-            string token = "MTA1Nzk4MjM2NzEyMzM5MDQ3NQ.GlsPM8.Ck5hDKptP1vqtZqWbIUSHOQ3X1yg25tIYlKxOM";
+            string token = DateBase.GetToken();
+            commands = new CommandService();
+            services = new ServiceCollection().AddSingleton(client).AddSingleton(commands).BuildServiceProvider();
 
             await client.LoginAsync(TokenType.Bot, token);
             await client.StartAsync();
             await Task.Delay(-1);
         }
 
-        private Task CommandsHandler(SocketMessage msg)
+        private async Task CommandsHandler(SocketMessage arg)
         {
-            if (!msg.Author.IsBot)
-            {
-                msg.Channel.SendMessageAsync(msg.Content);
-                return Task.CompletedTask;
-            }
+            var msg = arg as SocketUserMessage;
+            var context = new SocketCommandContext(client, msg);
+            string serverName = context.Guild.Name;
 
-            return Task.CompletedTask;
+            DateBase.CreateDateBase(serverName);
+
+            if (msg.Author.IsBot)
+                return;
+
+            await msg.Channel.SendMessageAsync($"{serverName}");
         }
 
         private Task Log(LogMessage msg)
