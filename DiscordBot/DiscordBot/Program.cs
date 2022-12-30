f40883b3fc2dbd3a9a7d04fcfdda3bb20abb678b
AxVol <ax_vol@mail.ru>
2022-12-30 20:13:48 +0300

    add simple commands

--- DiscordBot/DiscordBot/Program.cs
+++ DiscordBot/DiscordBot/Program.cs
@@ -4,6 +4,7 @@ using Discord.Commands;
 using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace DiscordBot
 {
@@ -30,6 +31,7 @@ namespace DiscordBot
             commands = new CommandService();
             services = new ServiceCollection().AddSingleton(client).AddSingleton(commands).BuildServiceProvider();
 
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
             await client.LoginAsync(TokenType.Bot, token);
             await client.StartAsync();
             await Task.Delay(-1);
@@ -40,13 +42,23 @@ namespace DiscordBot
             var msg = arg as SocketUserMessage;
             var context = new SocketCommandContext(client, msg);
             string serverName = context.Guild.Name;
+            int argPos = 0;
 
             DateBase.CreateDateBase(serverName);
 
             if (msg.Author.IsBot)
                 return;
 
-            await msg.Channel.SendMessageAsync($"{serverName}");
+            if (msg.HasStringPrefix("!", ref argPos))
+            {
+                var result = await commands.ExecuteAsync(context, argPos, services);
+                
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+
+                if (result.Error.Equals(CommandError.UnmetPrecondition))
+                    await msg.Channel.SendMessageAsync(result.ErrorReason);
+            }
         }
 
         private Task Log(LogMessage msg)
